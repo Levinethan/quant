dc61aba36b053643669412e11124f2dc93fc7d3b
Levinethan <levinethan@yahoo.com>
2025-03-26 02:38:35 +0000

    endpoint change

--- ML/candle.py
+++ ML/candle.py
@@ -15,32 +15,32 @@ provider_glassnode = "glassnode"
 # Time interval
 interval = "1d"
 # Exchange Platform
-exhange = "Binance"
+exchange = "Binance"
 
 ########################################################## coinglass endpoint ##################################################
 # ------------------------------------------------- liquidation -------------------------------------------------
-endpoint="futures/liquidation/history?exchange=Bybit&symbol=BTCUSDT&interval="+interval # 趋势
+endpoint="futures/liquidation/history?exchange="+exchange+"&symbol=BTCUSDT&interval="+interval # 趋势
 # ------------------------------------------------- liquidation -------------------------------------------------
 
 # ------------------------------------------------- takerBuySellVolume -------------------------------------------------
-endpoint2="futures/takerBuySellVolume/history?exchange=Bybit&symbol=BTCUSDT&interval="+interval # leading indicator
-endpoint3="futures/aggregatedTakerBuySellVolumeRatio/history?exchange=Bybit&symbol=BTCUSDT&interval="+interval # 不会用
+endpoint2="futures/takerBuySellVolume/history?exchange="+exchange+"&symbol=BTCUSDT&interval="+interval # leading indicator
+endpoint3="futures/aggregatedTakerBuySellVolumeRatio/history?exchange="+exchange+"&symbol=BTCUSDT&interval="+interval # 不会用
 # ------------------------------------------------- takerBuySellVolume -------------------------------------------------
 
 # ------------------------------------------------- openInterest -------------------------------------------------
-endpoint4="futures/openInterest/ohlc-history?exchange=Bybit&symbol=BTCUSDT&interval="+interval # lagging indicator openInterest 不能merge candle close
+endpoint4="futures/openInterest/ohlc-history?exchange="+exchange+"&symbol=BTCUSDT&interval="+interval # lagging indicator openInterest 不能merge candle close
 endpoint5="futures/openInterest/ohlc-aggregated-history?symbol=BTC&interval="+interval #提供市场规模、流动性、参与度和情绪的综合信息
 # ------------------------------------------------- openInterest -------------------------------------------------
 
 # ------------------------------------------------- fundingRate -------------------------------------------------
-endpoint6="futures/fundingRate/ohlc-history?exchange=Bybit&symbol=BTCUSDT&interval="+interval # lagging indicator
+endpoint6="futures/fundingRate/ohlc-history?exchange="+exchange+"&symbol=BTCUSDT&interval="+interval # lagging indicator
 endpoint7="futures/fundingRate/oi-weight-ohlc-history?symbol=BTC&interval="+interval # 不会用
 endpoint8="futures/fundingRate/vol-weight-ohlc-history?symbol=BTC&interval="+interval # 不会用
 # ------------------------------------------------- fundingRate -------------------------------------------------
 
 # ------------------------------------------------- LongShortAccountRatio -------------------------------------------------
-endpoint9="futures/globalLongShortAccountRatio/history?exchange=Bybit&symbol=BTCUSDT&interval="+interval # 提供市场持仓情绪
-endpoint10="futures/topLongShortPositionRatio/history?exchange=Binance&symbol=BTCUSDT&interval="+interval # leading indicator
+endpoint9="futures/globalLongShortAccountRatio/history?exchange="+exchange+"&symbol=BTCUSDT&interval="+interval # 提供市场持仓情绪
+endpoint10="futures/topLongShortPositionRatio/history?exchange="+exchange+"&symbol=BTCUSDT&interval="+interval # leading indicator
 # ------------------------------------------------- LongShortAccountRatio -------------------------------------------------
 ####################################################### coinglass endpoint #######################################################
 
@@ -50,7 +50,7 @@ class MyStrategy(Strategy):
     candle_data = []
     async def on_datasource_interval(self, strategy, topic, data_list):
         model = self.data_map[topic]
-        long_short_ratio = np.array(list(map(lambda c: float(c["longShortRatio"]), model)))
+        #long_short_ratio = np.array(list(map(lambda c: float(c["longShortRatio"]), model)))
         self.datasource_data.append(super().data_map[topic][-1])
  
     async def on_candle_closed(self, strategy, topic, symbol):
@@ -61,7 +61,7 @@ class MyStrategy(Strategy):
         candle_df = pd.DataFrame(self.candle_data)
         df = pd.merge(datasource_df,candle_df)
         # ---------------------- change endpoint ----------------------
-        name = re.sub(r'[^a-zA-Z0-9\s]', '', endpoint10) 
+        name = re.sub(r'[^a-zA-Z0-9\s]', '', endpoint) 
         # ---------------------- change endpoint ----------------------
         path = "./src/" + f"{provider_coinglass}-{name}.csv"
         print("path :",path)
@@ -71,7 +71,7 @@ class MyStrategy(Strategy):
 config = RuntimeConfig(
     mode=RuntimeMode.Backtest,
     datasource_topics=[
-        f"{provider_coinglass}|{endpoint10}"
+        f"{provider_coinglass}|{endpoint}"
         ],
     candle_topics=[
         "binance-linear|candle?symbol=BTCUSDT&interval="+interval
