f1a73c44985dfe3d23dbc7620c6edfafb15c111c
Ubuntu <azureuser@quant.phlikexwdr0evl5rycbxqemqmg.bx.internal.cloudapp.net>
2025-06-26 09:27:36 +0000

    longshortraito

--- ML/fetch_datasource.py
+++ ML/fetch_datasource.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import asyncio
 import cybotrade_datasource
 from datetime import datetime, timezone
@@ -8,22 +9,36 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = "CBfBYegqTPaE8qJukLTaBHgt9prjnZmR6Eqtv1rllbxgIdOy"
 
-
+# ------------------------------------------------------------- Coinglass -------------------------------------------------------------
 # TOPIC = 'bybit-linear|candle?symbol=BTCUSDT&interval=1d'
-# TOPIC = 'cryptoquant|btc/inter-entity-flows/miner-to-miner?from_miner=f2pool&to_miner=all_miner&window=hour'
-TOPIC = 'coinglass|coinbase-premium-index?interval=1h'
-#TOPIC = 'cryptoquant|eth/market-data/coinbase-premium-index?window=hour&exchange=binance'
+# TOPIC = 'coinglass|coinbase-premium-index?interval=1h'
+TOPIC = 'coinglass|futures/top-long-short-account-ratio/history?exchange=Binance&symbol=BTCUSDT&interval=1d'
+
+# ------------------------------------------------------------- Coinglass -------------------------------------------------------------
+
+# ------------------------------------------------------------- Cryptoquant -----------------------------------------------------------
+# cryptoquant_endpoint1 = 'cryptoquant|eth/market-data/coinbase-premium-index?window=hour&exchange=binance'
+# cryptoquant_endpoint2 = 'cryptoquant|btc/inter-entity-flows/miner-to-miner?from_miner=f2pool&to_miner=all_miner&window=hour'
+# ------------------------------------------------------------- Cryptoquant -----------------------------------------------------------
+
+# ------------------------------------------------------------- Glassnode -------------------------------------------------------------
+
+
+# ------------------------------------------------------------- Glassnode -------------------------------------------------------------
+
+
 async def main():
     data = await cybotrade_datasource.query_paginated(
         api_key=API_KEY, 
         topic=TOPIC, 
-        start_time=datetime(year=2024, month=1, day=1, tzinfo=timezone.utc),
-        end_time=datetime(year=2025, month=1, day=1, tzinfo=timezone.utc)
+        start_time=datetime(year=2023, month=1, day=1, tzinfo=timezone.utc),
+        end_time=datetime(year=2025, month=6, day=1, tzinfo=timezone.utc)
     )
     df = pd.DataFrame(data)
     df['start_time'] = pd.to_datetime(df['start_time'], unit='ms')
     print(df)
-    df.to_csv(f"src/{TOPIC.replace('|', '_').replace('?', '_').replace('&', '_').replace('-', '_').replace('=', '_')}.csv",index=False)
+    path = f"src/{TOPIC.replace('|', '_').replace('/','_').replace('?', '_').replace('&', '_').replace('-', '_').replace('=', '_')}.csv"
+    df.to_csv(path,index=False)
     
 
 asyncio.run(main())
