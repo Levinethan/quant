606916606a386f76aab1bf9045b353743d487191
Ubuntu <azureuser@quant.phlikexwdr0evl5rycbxqemqmg.bx.internal.cloudapp.net>
2025-06-26 08:01:59 +0000

    premiumIndex Coinbase

--- ML/doublewin_data.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-extra_info_url = "https://api2.bybit.com/s1/byfi/get-products-extra-info"
-
-resp = requests.post(url=extra_info_url, json={"product_type": 10})
-all_product_info = resp.json()["result"]["double_win_offers"]
-print("double win:",all_product_info)
---
+++ ML/fetch_datasource.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import asyncio
+import cybotrade_datasource
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = "CBfBYegqTPaE8qJukLTaBHgt9prjnZmR6Eqtv1rllbxgIdOy"
+
+
+# TOPIC = 'bybit-linear|candle?symbol=BTCUSDT&interval=1d'
+# TOPIC = 'cryptoquant|btc/inter-entity-flows/miner-to-miner?from_miner=f2pool&to_miner=all_miner&window=hour'
+TOPIC = 'coinglass|coinbase-premium-index?interval=1h'
+#TOPIC = 'cryptoquant|eth/market-data/coinbase-premium-index?window=hour&exchange=binance'
+async def main():
+    data = await cybotrade_datasource.query_paginated(
+        api_key=API_KEY, 
+        topic=TOPIC, 
+        start_time=datetime(year=2024, month=1, day=1, tzinfo=timezone.utc),
+        end_time=datetime(year=2025, month=1, day=1, tzinfo=timezone.utc)
+    )
+    df = pd.DataFrame(data)
+    print(df)
+    df.to_csv(f"output/{TOPIC.replace('|', '_').replace('?', '_').replace('&', '_').replace('-', '_').replace('=', '_')}.csv")
+    
+
+asyncio.run(main())
+
